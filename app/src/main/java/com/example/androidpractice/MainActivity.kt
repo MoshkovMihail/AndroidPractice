94332eb20c5cd50b0cc9e2c4b4fe81a33604161e
MoshkovMihail <mgmoshkov@kpfu.ru>
2025-12-01 19:49:00 +0300

    hw_4

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import com.example.androidpractice.screen.main_screen.MainScreen
 import com.example.androidpractice.ui.theme.AppTheme
 
@@ -15,7 +13,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppTheme {
                 Surface {
-                    MainScreen();
+                    MainScreen()
                 }
             }
         }
